0a4036cdb878838c4662a22111535b55dad2b91f
Tibolt <tibo.ak015@gmail.com>
2022-11-17 14:27:43 +0100

    added: ui to choose files and show plot

--- formula.py
+++ formula.py
@@ -23,41 +23,30 @@ def open_time(name):
     return result
 
 
-# A = l * h
+def calculate(l, h, num):
+    # A = l * h
+    # suma wszystkich czasow rozmow / dlugosc obserwacji np 1h
 
-# suma wszystkich czasow rozmow / dlugosc obserwacji np 1h
+    # l -czas trwania polaczenia w ciagu doby, l = [158, 9, 9, 3, 11, 89]
+    # h - kolejne minuty doby ,h = [1, 2, 4, 8, 11, 14]
+    sum = 0
+    for x in l:
+        sum += x
 
-# czas trwania polaczenia w ciagu doby
-# l = [158, 9, 9, 3, 11, 89]
-l = open_time("time.txt")
+    # sredni czas trwania polaczenia
+    lt = sum / len(l)
 
-# kolejne minuty doby
-# h = [1, 2, 4, 8, 11, 14]
-h = open_numbers("numbers.txt", 0)
+    # ilosc wywolan jakie zarejstrowano w danej minucie doby
+    # lambda
+    # num = [2.38095e-05, 7.14286e-05, 2.38095e-05, 2.38095e-05, 2.38095e-05, 4.7619e-05]
+    out = np.asarray(num) * lt
+    hours = np.asanyarray(h) / 60
 
-sum = 0
-for x in l:
-    sum += x
-
-# sredni czas trwania polaczenia
-lt = sum / len(l)
-
-# ilosc wywolan jakie zarejstrowano w danej minucie doby
-# lambda
-# num = [2.38095e-05, 7.14286e-05, 2.38095e-05, 2.38095e-05, 2.38095e-05, 4.7619e-05]
-num = open_numbers("numbers.txt", 1)
+    plt.plot(hours, out)
+    plt.xlabel("kolejne godziny doby")
+    plt.ylabel("sredni czas * ilosc wywolan")
+    plt.xticks([0, 4, 8, 12, 16, 20, 24])
+    plt.show()
 
 # wartosc chwilowa intensywnosci wywolan w poszczegolnych minutach - wzor:
 # num[h] / sum(num)
-
-
-out = np.asarray(num) * lt
-print(out)
-
-hours = np.asanyarray(h) / 60
-
-plt.plot(hours, out)
-plt.xlabel("kolejne godziny doby")
-plt.ylabel("sredni czas * ilosc wywolan")
-plt.xticks([0, 4, 8, 12, 16, 20, 24])
-plt.show()
--- main.py
+++ main.py
@@ -2,18 +2,24 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 from tkinter import *
 from tkinter.ttk import *
+from tkinter import filedialog as fd
+from tkinter.messagebox import showinfo
 import matplotlib
 import matplotlib.pyplot as plt
+import formula
+import os
+
 matplotlib.use("TkAgg")
 
-
 window = Tk()
-window.geometry('800x600')
+window.geometry("800x600")
+
 
 label = Label(window, text="Test Program")
 label.place(relx=0.5, rely=0.1, anchor="center")
 
 
+# For testing and learning
 def clicked():
     try:
         x = float(inpx.get())
@@ -38,31 +44,9 @@ def draw(x, y):
     plt.show()
 
 
-def formula():
-    # A = l * h
-    l = [158, 9, 9, 3]
-    h = [1, 2, 4, 8]
-    sum = 0
-    for x in l:
-        sum += x
-
-    lt = sum / 4
-    print(lt * h)
-
-
-def test():
-    # test wzoru A = l * h
-    l = [158, 9, 9, 3]
-    h = [1, 2, 4, 8]
-    draw(l, h)
-
-
 bt = Button(window, text="Multiply", command=clicked)
 # bt.grid(column=2, row=2)
-bt.place(relx=0.5, rely=0.3, anchor="center")
-
-debug_bt = Button(window, text="Debug", command=test)
-debug_bt.place(relx=0.5, rely=0.4, anchor="center")
+bt.place(relx=0.5, rely=0.25, anchor="center")
 
 inpx = Entry(window)
 inpx.place(relx=0.5, rely=0.15, anchor="center")
@@ -74,5 +58,77 @@ inpy.place(relx=0.5, rely=0.20, anchor="center")
 labely = Label(window, text="y")
 labely.place(relx=0.35, rely=0.20, anchor="center")
 
+
+# Actual program
+def select_file_time():
+    global l_file
+    filetypes = (("text files", "*.txt"), ("All files", "*.*"))
+
+    filename = fd.askopenfilename(
+        title="Open a file", initialdir="~", filetypes=filetypes
+    )
+    l_file = filename
+    # check if filename is not empty, user could decide to not pick file
+    if type(filename) == str:
+        time_file_label.config(text=os.path.basename(filename))
+
+
+def select_file_numbers():
+    global h_file
+    filetypes = (("text files", "*.txt"), ("All files", "*.*"))
+
+    filename = fd.askopenfilename(
+        title="Open a file", initialdir="~", filetypes=filetypes
+    )
+    h_file = filename
+    if type(filename) == str:
+        numbers_file_label.config(text=os.path.basename(filename))
+
+
+def show():
+    global l_file, h_file
+    global l, h, num
+    try:
+        l = formula.open_time(l_file)
+        h = formula.open_numbers(h_file, 0)
+        num = formula.open_numbers(h_file, 1)
+        formula.calculate(l, h, num)
+    except:
+        openNewWindow("Error", "200x100", "Error with input files")
+        # DEBUG
+        print("Error with files")
+
+
+# function to open a new window
+def openNewWindow(title, size, text):
+
+    # Toplevel object which will
+    # be treated as a new window
+    newWindow = Toplevel(window)
+
+    # sets the title of the
+    # Toplevel widget
+    newWindow.title(title)
+
+    # sets the geometry of toplevel
+    newWindow.geometry(size)
+
+    # A Label widget to show in toplevel
+    Label(newWindow, text=text).pack()
+
+
+# open button
+open_button = Button(window, text="Open time.txt", command=lambda: select_file_time())
+open_button.place(relx=0.2, rely=0.35, anchor="center")
+time_file_label = Label(window, text="Empty")
+time_file_label.place(relx=0.2, rely=0.4, anchor="center")
+
+open_button = Button(window, text="Open  numbers.txt", command=lambda: select_file_numbers())
+open_button.place(relx=0.5, rely=0.35, anchor="center")
+numbers_file_label = Label(window, text="Empty")
+numbers_file_label.place(relx=0.5, rely=0.4, anchor="center")
+
+open_button = Button(window, text="show plot", command=show)
+open_button.place(relx=0.35, rely=0.45, anchor="center")
 
 window.mainloop()
